cfc78288a3c2db41b435ffcee1abc30eb3838788
Sahil2375 <sp2930514@gmail.com>
2025-08-05 22:53:04 +0530

    22764

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
-from .views import *
+from django.urls import path, include
+from django.contrib import admin
+from django.shortcuts import render
+
+def custom_404_view(request, exception):
+    return render(request, '404.html', status=404)
 
 urlpatterns = [
-    
+    path('admin/', admin.site.urls),
+    path('', include('home.urls')),
 ]
+
+handler404 = 'restaurant_management.urls.custom_404_view'
