f73b73a2d16b90ee50273183322a7a73f3bccc82
Sahil2375 <sp2930514@gmail.com>
2025-08-06 15:17:33 +0530

    22767

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Restaurant
+from django.conf import settings
 
 # Create your views here.
+
+def homepage_view(request):
+    restaurant = Restaurant.objects.first()   # Fetch the first restaurant.
+    return render(request, 'Homepage.html', {'restaurant_name': restaurant.name if restaurant else 'Restaurant'})
+    return render(request, 'Homepage.html', {'restaurant_name': settings.RESTAURANT_NAME})
