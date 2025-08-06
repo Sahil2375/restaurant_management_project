e3bd1fbb46e8a753a4a8f26d67f420321c6f6652
Sahil2375 <sp2930514@gmail.com>
2025-08-06 17:16:54 +0530

    Auto-save orders/urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from . import views
 from .views import homepage_view, about_view
 
 urlpatterns = [
     path('', homepage_view, name='homepage'),
     path('about/', about_view, name='about'),
+    path('', views.homepage, name='homepage'),
+
 ]
