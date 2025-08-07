75d47aa63f06a0c53175e698d34eeed0f0f85d83
Sahil2375 <sp2930514@gmail.com>
2025-08-07 19:18:22 +0530

    23893

--- products/views.py
+++ products/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from 
 
 from .models import Item
 from .serializers import ItemSerializer
