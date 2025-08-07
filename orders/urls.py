672cdce96d237e114a8efec596a03c556a437ef1
Sahil2375 <sp2930514@gmail.com>
2025-08-07 20:13:22 +0530

    25814

--- orders/urls.py
+++ orders/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import homepage_view, about_view, homepage_views
+from .views import homepage_view, about_view, homepage_views, reservations_view
 
 urlpatterns = [
     path('', homepage_view, name='homepage'),
     path('about/', about_view, name='about'),
     path('', views.homepage, name='homepage'),
     path('', homepage_views, name='home'),
+    path('reservations/', reservations_view, name='reservations'),
 ]
