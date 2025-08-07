cf2bdba3f55e542014f842303d41623a0636ad88
Sahil2375 <sp2930514@gmail.com>
2025-08-07 19:21:07 +0530

    23893

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from .models import Restaurant
 
+from django.conf import settings
+
 # Create your views here.
 
 def homepage(request):
     restaurant = Restaurant.objects.first()  # Fetch the first restaurant
     return render(request, 'index.html', {'restaurant_name': restaurant.name if restaurant else 'Restaurant'})
+
+def 
+
+def homepage_view(request):
+    phone_number = settings.RESTAURANT_PHONE
+    return render(request, 'home.html', {'phone': phone_number})
