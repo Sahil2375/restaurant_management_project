d393f0893d1e55859c14382ef4135c20834d8b92
Sahil2375 <sp2930514@gmail.com>
2025-08-07 20:12:00 +0530

    25814

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import menu_list_view, contact_us_view
+from .views import menu_list_view, contact_us_view, reservations_view
 
 urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
     path('menu/', menu_list_view, name='menu-list'),
     path('contact', contact_us_view, name='contact-us'),
+    path('reservations/', reservations_view, name='reservations'),
 ]
