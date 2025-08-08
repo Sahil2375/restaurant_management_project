7c37c48ea233d1b8298f888c92991e7bf68c6bd7
Sahil2375 <sp2930514@gmail.com>
2025-08-08 13:50:27 +0530

    25962

--- home/views.py
+++ home/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import FeedbackForm
+from datetime import datetime
+
 from .models import Restaurant
 
 # Create your views here.
@@ -7,3 +10,18 @@ def homepage(request):
     restaurant = Restaurant.objects.first()   # Fetch the first restaurant.
     restaurant_name = restaurant.name if restaurant else "Default Restaurant"
     return render(request, 'index.html', {'restaurant_name': restaurant.name})
+
+def feedback_view(request):
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('feedback')  # Redirect after saving.
+
+        else:
+            form = FeedbackForm()
+
+        return render(request, 'feedback.html', {
+            'form': form,
+            'current_year': datetime.now().year
+        })
