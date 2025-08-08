0a6a777ee2d9fbd9846e812f9fb601564eed527d
Sahil2375 <sp2930514@gmail.com>
2025-08-08 14:10:44 +0530

    25965

--- home/models.py
+++ home/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-class Restaurant(models.Model):
-    name = models.CharField(max_length=100)
+class Feedback(models.Model):
+    "Stores customer feedback"
+    comments = models.TextField(help_text="Customer feedback comments.")
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return f"Feedback #{self.id} - {self.created_at.strftime('%Y-%m-%d')}"
