32c4eabb96c7ff692ae2de38ced64b95154dcb2b
Sahil2375 <sp2930514@gmail.com>
2025-08-11 21:01:38 +0530

    28299

--- home/models.py
+++ home/models.py
@@ -25,3 +25,11 @@ class MenuItem(models.Model):
 
     def __str__(self):
         return self.name
+
+class ContactSubmission(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"
