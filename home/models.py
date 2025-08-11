37319dc244909dcb673f6f57f23f579827b237c4
Sahil2375 <sp2930514@gmail.com>
2025-08-11 20:44:21 +0530

    28297

--- home/models.py
+++ home/models.py
@@ -9,3 +9,19 @@ class Feedback(models.Model):
 
     def __str__(self):
         return f"Feedback #{self.id} - {self.created_at.strftime('%Y-%m-%d')}"
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    # NEW FIELD FOR IMAGE UPLOAD
+    image = models.ImageField(
+        upload_to='menu_images/',  # Folder inside MEDIA_ROOT
+        blank=True,
+        null=True,
+        help_text='Upload an image of the menu items'
+    )
+
+    def __str__(self):
+        return self.name
