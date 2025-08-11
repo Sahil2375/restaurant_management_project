17e9f6e6f5de51e807fe35ede203529cd3ff86b7
Sahil2375 <sp2930514@gmail.com>
2025-08-11 21:11:10 +0530

    28300

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import RestaurantInfo
 
 # Register your models here.
+
+@admin.register(RestaurantInfo)
+class RestaurantInfoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address')
