57adf51300ed794183d1b4722fa78e517321bd5d
Sahil2375 <sp2930514@gmail.com>
2025-08-11 21:39:03 +0530

    28303

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import RestaurantInfo
+from .models import RestaurantInfo, MenuItem
 
 # Register your models here.
 
 @admin.register(RestaurantInfo)
 class RestaurantInfoAdmin(admin.ModelAdmin):
     list_display = ('name', 'address')
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
