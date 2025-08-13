5dfdcc8dac28f54164dafba6339961259ce92972
Sahil2375 <sp2930514@gmail.com>
2025-08-13 13:02:33 +0530

    30646

--- account/views.py
+++ account/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
 
 # Create your views here.
+
+def home(request):
+    current_datetime = timezone.now()
+    return render(request, 'home.html', {
+        'current_datetime': current_datetime
+    })
