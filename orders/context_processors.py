a2dc7a9efdebfac5a4067ba9e4f74f5a6f1783f1
Sahil2375 <sp2930514@gmail.com>
2025-08-13 12:14:44 +0530

    30643

--- orders/context_processors.py
+++ orders/context_processors.py
@@ -1,4 +1,16 @@
-from datetime import datetime
+def breadcrumbs(request):
+    # Generates a list of breadcrumbs items based on the request path.
+    # Each breadcrumb is a dictionary with 'name' and 'url'.
+    path = request.path.strip("/").split("/")
+    breadcrumbs_list = []
+    accumulated_path = ""
 
-def current_year(request):
-    return {'current_year': datetime.now().year}
+    for segment in path:
+        if segment: # avoid empty strings
+            accumulated_path += f"/{segment}"
+            breadcrumbs_list.append({
+                "name": segment.replace("-", " ")..capitalize(),
+                "url": accumulated_path
+            })
+
+    return {"breadcrumbs": breadcrumbs_list}
