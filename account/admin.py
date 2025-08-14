5db8efc31b1470113b172dde7f72481f83de5e8c
Sahil2375 <sp2930514@gmail.com>
2025-08-14 15:00:23 +0530

    32038

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import RestaurantInfo
+from .models import RestaurantInfo, TodaysSpecial
 
 # Register your models here.
 
 admin.site.register(RestaurantInfo)
+
+admin.site.register(TodaysSpecial)
