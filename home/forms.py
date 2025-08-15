8ce063d38c05f7af4a3bea8f1d9ce4879398f3b3
Sahil2375 <sp2930514@gmail.com>
2025-08-15 17:24:12 +0530

    32040

--- home/forms.py
+++ home/forms.py
@@ -4,11 +4,12 @@ from .models import Feedback, ContactMessage
 class FeedbackForm(forms.ModelForm):
     class Meta:
         model = Feedback
-        fields = ['name', 'feedback']
+        fields = ['name', 'feedback_text']
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your Name'}),
-            'feedback': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Your Feedback'}),
+            'name': forms.TextInput(attrs={'placeholder': 'Your Name'}),
+            'feedback_text': forms.Textarea(attrs={'placeholder': 'Your Feedback'}),
         }
+        
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100, required=True)
     email = forms.EmailField(required=True) # Validates email format automatically.
