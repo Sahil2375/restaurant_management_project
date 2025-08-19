e28c3aab93174d4f7a5e77ba790930d76de76fce
Sahil2375 <sp2930514@gmail.com>
2025-08-19 15:56:28 +0530

    35514

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.mail import send_mail
+from django.core.paginator import Paginator
 from django.conf import settings
 from django.contrib import messages
 from .forms import FeedbackForm, ContactForm
@@ -76,9 +77,14 @@ def feedback_view(request):
         return render(request, 'feedback.html', {'form': form})
 
 def menu_view(request):
-    # Display all menu items on the menu page.
-    menu_items = MenuItem.objects.all()
-    return render(request, 'menu.html', {'menu_items': menu_items})
+    menu_items = MenuItem.objects.all().order_by("name")  # fetch all items
+
+    # Paginate with 5 items per page (you can change this)
+    paginator = Paginator(menu_items, 5)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'menu.html', {'page_obj': page_obj})
 
 def contact_view(request):
     if request.method == 'POST':
