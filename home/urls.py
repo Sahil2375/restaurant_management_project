22e7491157116fdbf11972f7f6dbb976bf25d4a6
Sahil2375 <sp2930514@gmail.com>
2025-08-20 10:42:39 +0530

    35853

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from django.contrib import admin
 from django.shortcuts import render
+from . import views
 
 def custom_404_view(request, exception):
     return render(request, '404.html', status=404)
@@ -8,6 +9,7 @@ def custom_404_view(request, exception):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('', views.homepage, name='homepage'),
 ]
 
 handler404 = 'home.urls.custom_404_view'
