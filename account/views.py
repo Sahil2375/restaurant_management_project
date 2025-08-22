17b9aa4a3d17acbe9d24d8abef9d09fe123746ff
Sahil2375 <sp2930514@gmail.com>
2025-08-22 21:52:04 +0530

    37829

--- account/views.py
+++ account/views.py
@@ -8,3 +8,12 @@ def home(request):
     return render(request, 'home.html', {
         'current_datetime': current_datetime
     })
+
+def restaurant_gallery(request):
+    images = [
+        "https://example.com/image1.jpg",
+        "https://example.com/image2.jpg",
+        "https://example.com/image3.jpg",
+        "https://example.com/image4.jpg",
+    ]
+    return render(request, "restaurant/gallery.html", {"images": images})
