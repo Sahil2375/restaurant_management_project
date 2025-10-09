d97a75f1709ab2554b898aefc302536af58c9dbc
Sahil2375 <sp2930514@gmail.com>
2025-10-09 15:57:59 +0530

    Successfully created a Table model and also accessible and manageable through Django's built-in administration interface.

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import RestaurantInfo, MenuItem, Feedback, Chef
+from .models import RestaurantInfo, MenuItem, Feedback, Chef, Table
 
 # Register your models here.
 
@@ -18,3 +18,5 @@ class FeedbackAdmin(admin.ModelAdmin):
 @admin.register(Chef)
 class ChefAdmin(admin.ModelAdmin):
     list_display = ("name",)
+
+admin.site.register(Table)
