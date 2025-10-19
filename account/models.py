890183fb69e9ff1e255db1196fbad1e9ec2f2eb4
Sahil2375 <sp2930514@gmail.com>
2025-10-19 20:29:57 +0530

    Implemented the CustomerProfile inside the account/models.py successfully.

--- account/models.py
+++ account/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -32,3 +33,12 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
+    
+class CustomerProfile(models.Model):
+    # Link each profile to a Django user account
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='customer_profile')
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    delivery_address = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Customer Profile"
