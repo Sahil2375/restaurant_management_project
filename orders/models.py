e6d2d26f46ee66783aa30929511e64b9e0fbbd9e
Sahil2375 <sp2930514@gmail.com>
2025-10-20 11:39:26 +0530

    Added a new field created_at to Order model in orders/models.py

--- orders/models.py
+++ orders/models.py
@@ -121,7 +121,7 @@ class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     # short_id = models.CharField(max_length=20, unique=True)
     # order_id = models.CharField(max_length=20, unique=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
     customer_name = models.CharField(max_length=100, blank=True, null=True)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
